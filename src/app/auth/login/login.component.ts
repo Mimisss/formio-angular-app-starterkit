21c9800ca5265d34c49bba0a9558fc11aca9c1e8
Dimitris Pantazopoulos <pantazop.dim@gmail.com>
2022-08-16 16:11:12 +0300

    Added dynamic language translations from resource

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormioAuthLoginComponent, FormioAuthService } from '@formio/angular/auth';
 import { Formio } from 'formiojs';
+import { AppConfig } from 'src/app/config';
 
 @Component({
   selector: 'app-login',
@@ -16,12 +17,22 @@ export class LoginComponent extends FormioAuthLoginComponent implements OnInit {
   ngOnInit(): void {
     // fetch the user login form from the resource
     Formio.createForm(document.getElementById('formio'), this.service.loginForm, {
-      language: 'el',
-      i18n: {
-        el: {
-          'Email': 'Ηλεκτρονικό ταχυδρομείο'
-        }
-      }
+      language: 'en',
+    }).then((form) => {
+
+      Formio.fetch(AppConfig.appUrl + '/language/submission?data.language=el').then((response) => {
+        response.json().then((result) => {
+          console.log(result[0].data.translations);
+          // convert array of { 'translationsKey': 'translationsTranslation' } to an object
+          let translations = result[0].data.translations.reduce((obj, current) => {
+            obj[current.translationsKey] = current.translationsTranslation;
+            return obj;
+          }, {});          
+          console.log(translations);
+          form.addLanguage('el', translations);
+          form.language = 'el';
+        });        
+      });
     });
   }
 
